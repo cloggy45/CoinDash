213d985d4b68a2b32604478fb545a6db84edec8a
Paul Mcloughlin <cloggy45@users.noreply.github.com>
2018-04-02 01:21:09 +0100

    Added basic styles for select component

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
+
+const Section = styled.section`
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+  margin: 10px;
+  background: #265566;
+  color: #2d8490;
+`;
+
+const SelectBox = styled.select`
+  align: flex-end;
+  background: #21364a;
+  color: #265566;
+  border: none;
+  margin: 0 10px;
+`;
 
 const Select = props => {
   const { handleChange, value, selected, label, list } = props;
   return (
-    <section>
+    <Section>
       <label>{label}</label>
-      <select value={selected} onChange={handleChange}>
+      <SelectBox value={selected} onChange={handleChange}>
         {list.map((data, index) => {
           return (
             <option value={value[index]} key={data.toString()}>
@@ -15,15 +33,9 @@ const Select = props => {
             </option>
           );
         })}
-      </select>
-    </section>
+      </SelectBox>
+    </Section>
   );
-};
-
-Select.propTypes = {
-  label: PropTypes.string,
-  list: PropTypes.array,
-  selected: PropTypes.string
 };
 
 export default Select;
