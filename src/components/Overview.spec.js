6efe4f2d0c756ee2146d215d46e5f212c4070e72
Paul Mcloughlin <cloggy45@users.noreply.github.com>
2018-05-13 03:53:34 +0100

    Test Boilerplate
    Added test boilerplate

--- src/components/Overview.spec.js
+++ src/components/Overview.spec.js
@@ -1,13 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { shallow, mount, render } from "enzyme";
+import { shallow, mount, render, configure } from "enzyme";
 import renderer from "react-test-renderer";
+
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
 
 import { Overview } from "../components/Overview";
 
 describe("Overview Component", () => {
+  let props;
+  let mountedComponent;
+  const overview = () => {
+    if (!mountedComponent) {
+      mountedComponent = mount(<Overview {...props} />);
+    }
+    return mountedComponent;
+  };
+
+  beforeEach(() => {
+    props = {
+      overview: undefined,
+      fetch: undefined
+    };
+    mountedComponent = undefined;
+  });
+
   it("Renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Overview />, div);
+  });
+
+  it("Renders a renders", () => {
+    const section = overview().find("div");
+    expect(section.length).toBeGreaterThan(0);
   });
 });
