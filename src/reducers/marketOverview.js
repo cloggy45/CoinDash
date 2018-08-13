e4bd49781de8eedb5193cb5768b42e4a35658653
paul mcloughlin <cloggy45@googemail.com>
2018-08-13 08:03:18 +0100

    Added market overview reducer and tests

---
+++ src/reducers/marketOverview.js
@@ -0,0 +1,30 @@
+const initState = {
+    isFetching: true,
+    marketOverview: [],
+    error: null
+};
+
+export const marketOverview = ( currentState = initState, action ) => {
+    switch(action.type) {
+        case "FETCH_MARKET_OVERVIEW_REQUEST":
+            return {
+                ...currentState,
+                isFetching: true
+            };
+        case "FETCH_MARKET_OVERVIEW_SUCCESS":
+            return {
+                ...currentState,
+                isFetching: false,
+                marketOverview: action.payload
+            };
+
+        case "FETCH_MARKET_OVERVIEW_FAILED":
+            return {
+                ...currentState,
+                isFetching: false,
+                error: action.payload
+            }
+        default:
+            return currentState;
+    }
+};
--- src/reducers/marketOverview.spec.js
+++ src/reducers/marketOverview.spec.js
@@ -1,3 +1,38 @@
 import { marketOverview } from './marketOverview';
 import expect from 'expect';
-import { } from '../actions/actionTypes';
+import { FETCH_MARKET_OVERVIEW_FAILED, FETCH_MARKET_OVERVIEW_REQUEST, FETCH_MARKET_OVERVIEW_SUCCESS } from '../actions/actionTypes';
+
+describe('Market Overview Reducer', () => {
+    it('should return the initial state', () => {
+        expect(marketOverview(undefined, {})).toEqual({
+            isFetching: true,
+            marketOverview: [],
+            error: null
+        });
+    });
+
+    it('should handle FETCH_MARKET_OVERVIEW_REQUEST', () => {
+        const someAction = {
+            type: FETCH_MARKET_OVERVIEW_REQUEST,
+            isFetching: true
+        };
+
+        expect(marketOverview({}, someAction)).toEqual({
+            isFetching: true
+        })
+    });
+
+    it('should handle FETCH_MARKET_OVERVIEW_SUCCESS', () => {
+        const someAction = {
+            type: FETCH_MARKET_OVERVIEW_SUCCESS,
+            isFetching: false,
+            payload: ["Some Data"]
+        };
+
+        expect(marketOverview({}, someAction)).toEqual({
+            isFetching: false,
+            marketOverview: ["Some Data"]
+        });
+    })
+
+});
