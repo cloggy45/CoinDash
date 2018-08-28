a00b777adca6d757f0d276f5592e3b906622b5c8
paul mcloughlin <cloggy45@googemail.com>
2018-08-28 01:31:49 +0100

    Split firebase actions from api actions@

--- src/actions/api.js
+++ src/actions/api.js
@@ -13,30 +13,6 @@ import {
 } from './actionTypes';
 
 
-import {watchListRef} from '../firebase.js';
-
-export const addToWatchList = (coin, uid) => {
-    return dispatch => {
-        watchListRef.child(uid).push().set(coin);
-    }
-};
-
-export const fetchWatchList = uid => {
-    return dispatch => {
-        dispatch({
-            type: FETCH_WATCHLIST_REQUEST,
-            isFetching: true
-        });
-        watchListRef.child(uid).on("value", snapshot => {
-            dispatch({
-                type: FETCH_WATCHLIST_SUCCESS,
-                payload: snapshot.val()
-            })
-        })
-    }
-}
-
-
 export const receiveCoinData = json => {
     return {
         type: RECEIVE_COIN_DATA,
