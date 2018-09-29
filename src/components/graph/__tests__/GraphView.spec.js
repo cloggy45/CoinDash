a952e87ced97fcf078a466a045deb326c79e8a9f
cloggy45 <cloggy45@googlemail.com>
2018-09-28 23:19:43 -0400

    Refactored test, added test for graph type

--- src/components/graph/__tests__/GraphView.spec.js
+++ src/components/graph/__tests__/GraphView.spec.js
@@ -10,12 +10,29 @@ Enzyme.configure({adapter: new Adapter()});
 import {Graph, styles, GraphHolder} from '../GraphView';
 
 describe('Graph Component', () => {  
-
-  it('Should render bar graph without error', () => {
-    const wrapper = shallow(<Graph fetchCoinHistory={jest.fn()} classes={styles.card} />);
+  
+  function setupComponent(type="") {
+    return shallow(<Graph graphType={type} fetchCoinHistory={jest.fn()} classes={styles.card} />);
+  }
+    
+  it('Should render Bar graph without error', () => {
+    const wrapper = setupComponent('Bar');
     expect(wrapper.dive().find('Bar').exists()).toBe(true);
   });
+  
+  it('Should render Line graph without error', () => {
+    const wrapper = shallow(<Graph graphType={'Line'} fetchCoinHistory={jest.fn()} classes={styles.card} />);
+    expect(wrapper.dive().find('Line').exists()).toBe(true);
+  });
+  
+    it('Should render Doughnut graph without error', () => {
+    const wrapper = shallow(<Graph graphType={'Doughnut'} fetchCoinHistory={jest.fn()} classes={styles.card} />);
+    expect(wrapper.dive().find('Doughnut').exists()).toBe(true);
+  });
+  
 });
+
+
 
 describe('Graph Holder Component', () => {
   
