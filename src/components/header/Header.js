37e7795a5c166adae7854a292b147a3219ec19c6
Paul Mcloughlin <cloggy45@googlemail.com>
2018-10-03 06:19:59 +0100

    Added callback function to fetch watchlist from firebase

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,28 @@
-
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import {withStyles} from '@material-ui/core/styles';
 
 import Header, {styles} from './HeaderView';
 
-import { signIn, signOut, fetchUser } from '../../actions/auth';
+import {signIn, signOut, fetchUser} from '../../actions/auth';
 
-import { getAuthStatus, getLoadingStatus, getUserProfile } from '../../reducers/auth/authSelectors';
+import {getAuthStatus, getLoadingStatus, getUserProfile} from '../../reducers/auth/authSelectors';
+
+import {fetchWatchList} from '../../actions/firebase';
 
 const mapStateToProps = store => {
     return {
-        userProfile : getUserProfile(store),
-        isAuthorisedUser : getAuthStatus(store),
-        isLoading : getLoadingStatus(store)
+        userProfile: getUserProfile(store),
+        isAuthorisedUser: getAuthStatus(store),
+        isLoading: getLoadingStatus(store)
     }
 };
 
 const mapDispatchToProps = dispatch => ({
-  logon: () => dispatch(signIn()),
-  logout: () => dispatch(signOut()),
-  fetchUser: () => dispatch(fetchUser())
+    logon: () => dispatch(signIn()),
+    logout: () => dispatch(signOut()),
+    fetchUser: () => dispatch(fetchUser()),
+    fetchWatchList: (uid) => dispatch(fetchWatchList(uid))
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Header)));
