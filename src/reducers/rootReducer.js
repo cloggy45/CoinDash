6d1f216177b1a56cc915f3954f3b1bf7e3029b5a
paul mcloughlin <cloggy45@googemail.com>
2018-10-03 02:38:27 +0100

    Added coinMetaInfo and coinPriceInfo reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,9 +2,12 @@ import {combineReducers} from 'redux';
 import * as fromApi from './api';
 import * as fromTopTen from './topTen/topTen';
 import * as fromAuth from './auth/auth';
-import * as fromCoinHistory from './coinHistory/coinHistory';
 import * as fromSelected from './selected/selected';
 import * as fromMarketOverview from './marketOverview/marketOverview';
+
+import * as fromCoinHistory from './coinHistory/coinHistory';
+import * as fromCoinPriceInfo from './coinPriceInfo/coinPriceInfo';
+import * as fromCoinMetaInfo from './coinMetaInfo/coinMetaInfo';
 
 const API = 'API';
 const TOP_TEN = 'TOP_TEN';
@@ -12,14 +15,19 @@ const AUTH = 'AUTH';
 const COIN_HISTORY = 'COIN_HISTORY';
 const SELECTED = 'SELECTED';
 const MARKET_OVERVIEW = 'MARKET_OVERVIEW';
+const COIN_META_INFO = 'COIN_META_INFO';
+const COIN_PRICE_INFO = 'COIN_PRICE_INFO';
+
 
 const rootReducer = combineReducers({
     [AUTH]: fromAuth.auth,
     [API]: fromApi.api,
-    [COIN_HISTORY]: fromCoinHistory.coinHistory,
     [TOP_TEN]: fromTopTen.topTen,
     [SELECTED] : fromSelected.selected,
-    [MARKET_OVERVIEW] : fromMarketOverview.marketOverview
+    [MARKET_OVERVIEW] : fromMarketOverview.marketOverview,
+    [COIN_HISTORY]: fromCoinHistory.coinHistory,
+    [COIN_PRICE_INFO] : fromCoinPriceInfo.coinPriceInfo,
+    [COIN_META_INFO] : fromCoinMetaInfo.coinMetaInfo
 });
 
 
