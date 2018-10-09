65ae0b75767fba457b18c434c989d8e315eca084
Paul Mcloughlin <cloggy45@googlemail.com>
2018-10-09 05:01:54 +0100

    Added support for Redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,21 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
 
 export default function configureStore(preloadedState) {
     const middleware = [thunkMiddleware];
+    let composeEnhancers;
 
     if (process.env.NODE_ENV !== 'production') {
         console.log('Development Mode Engaged');
+        composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+            ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+            : compose;
         middleware.push(logger);
     }
 
-    const store = createStore(
-        rootReducer,
-        preloadedState,
-        applyMiddleware(...middleware)
-    );
+    const enhancer = composeEnhancers(applyMiddleware(...middleware));
+
+    const store = createStore(rootReducer, preloadedState, enhancer);
 
     if (module.hot) {
         module.hot.accept('../reducers/rootReducer', () => {
